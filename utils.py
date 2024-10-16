a8d519fb6b752be29e5d0d308f12a2207aadb6df
Mahfoudh94 <mouhamedali5@gmail.com>
2024-10-16 17:14:10 +0100

    modifying the memory/time function for better extensibility and maintainability

--- utils.py
+++ utils.py
@@ -1,7 +1,20 @@
 import functools
 
 from memory_profiler import memory_usage
+import inspect
 import time
+from memory_profiler import memory_usage
+class Counter:
+    def __init__(self):
+        self.__count = 0
+
+    def increment(self):
+        self.__count += 1
+
+    @property
+    def count(self):
+        return self.__count
+
 
 def time_and_space_profiler(func):
     @functools.wraps(func)
@@ -9,10 +22,14 @@ def time_and_space_profiler(func):
         start_time = time.time()
         mem_before = memory_usage()[0]
 
-        result = func(*args, **kwargs)
+        counter = Counter()
+        if 'compare_counter' in (inspect.getfullargspec(func).args + inspect.getfullargspec(func).kwonlyargs):
+            kwargs['compare_counter'] = counter
+        _result = func(*args, **kwargs)
 
         mem_after = memory_usage()[0]
         end_time = time.time()
 
-        return (result, end_time - start_time, mem_after - mem_before)
+        return counter.count, end_time - start_time, mem_after - mem_before
+
     return wrapper
