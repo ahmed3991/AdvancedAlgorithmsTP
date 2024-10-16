13cfda9723b1362362b3a4a9f9bdf34dc1738436
Mahfoudh94 <mouhamedali5@gmail.com>
2024-10-16 17:19:00 +0100

    updating the algorithms to return the intended result instead of comparison count, but accept and use the new counting method

--- algos.py
+++ algos.py
@@ -1,50 +1,52 @@
 from utils import time_and_space_profiler
 
-@time_and_space_profiler
-def SequentialSearch(arr, x):
-    for i in range(len(arr)):
-        if arr[i] == x:
-            return i + 1
-    return len(arr)
 
-@time_and_space_profiler
-def SequentialSearchOptimized(arr, x):
+def SequentialSearch(arr, x, *, compare_counter=None):
     for i in range(len(arr)):
+        compare_counter.increment() if compare_counter else None
         if arr[i] == x:
-            return i + 1
+            return i
+    return -1
+
+
+def SequentialSearchOptimized(arr, x, *, compare_counter=None):
+    for i in range(len(arr)):
+        compare_counter.increment() if compare_counter else None
+        if arr[i] == x:
+            return i
         elif arr[i] > x:
-            return i + 1
-    return len(arr)
+            return -1
+    return -1
 
-@time_and_space_profiler
-def BinarySearch(arr, x):
+
+def BinarySearch(arr, x, *, compare_counter=None):
     l = 0
     r = len(arr) - 1
-    comparisons = 0
     while l <= r:
-        comparisons += 1
+        compare_counter.increment() if compare_counter else None
         mid = l + (r - l) // 2
         if arr[mid] == x:
-            return comparisons
+            return mid
         elif arr[mid] < x:
             l = mid + 1
         else:
             r = mid - 1
-    return comparisons
+    return mid
 
-@time_and_space_profiler
-def BinarySearchRecursive(arr, x):
-    def inner(arr, x, l = 0, r = None):
+
+def BinarySearchRecursive(arr, x, *, compare_counter=None):
+    def inner(arr, x, l=0, r=None):
         if r is None:
             r = len(arr) - 1
         if l <= r:
             mid = l + (r - l) // 2
+            compare_counter.increment() if compare_counter else None
             if arr[mid] == x:
-                return 1
+                return mid
             elif arr[mid] < x:
-                return 1 + inner(arr, x, mid + 1, r)
+                return inner(arr, x, mid + 1, r)
             else:
-                return 1 + inner(arr, x, l, mid - 1)
-        return 1
+                return inner(arr, x, l, mid - 1)
+        return -1
 
     return inner(arr, x)
