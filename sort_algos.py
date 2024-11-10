ba7db615f07b3d9a1c8c1561eb612879bcbbfbe8
Abderrahmane Henna <henna1abdou@gmail.com>
2024-11-10 08:56:43 +0100

    test

--- sort_algos.py
+++ sort_algos.py
@@ -6,7 +6,22 @@
 
 ## TODO: Sort Algorithms implementations
 ## TODO: Selection Sort is our task now
+def selection_sort(arr):
+    comparison_count = 0
+    move_count = 0
+    arr = arr.copy()
 
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            comparison_count += 1
+            if arr[j] < arr[min_index]:
+                min_index = j
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            move_count += 1
+
+    return comparison_count, move_count
 
 ## TODO: make Benchmarks
 
