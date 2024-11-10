e22a42c3db7e3cab64b557997feb9a05976f5821
soufiane-developer <soufianehammadi02@gamil.com>
2024-11-10 09:21:42 +0100

    work

--- sort_algos.py
+++ sort_algos.py
@@ -7,7 +7,23 @@
 ## TODO: Sort Algorithms implementations
 ## TODO: Selection Sort is our task now
 
+t =[10,2,1,14,8,7,6,9]
 
+def sort_selection (t):
+    i=0
+    swap =0
+    comp = 0
+    for i in range(len(t)):
+        j=i
+        for j in range(len(t)):
+          comp = comp + 1
+          if t[j]>t[i] :
+              c= t[i]
+              t[i]=t[j]
+              t[j]=c
+              swap=swap+1
+    return 'sort_selection', comp , swap
+print(sort_selection (t))
 ## TODO: make Benchmarks
 
 print('hello')
