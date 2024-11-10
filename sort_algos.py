6c63ed5b46eaafb428c2ddbde82057a4a9b3e868
oualidl <largotoualid@gmail.com>
2024-11-10 11:04:36 +0100

    sort__

--- sort_algos.py
+++ sort_algos.py
@@ -6,6 +6,20 @@
 
 ## TODO: Sort Algorithms implementations
 ## TODO: Selection Sort is our task now
+def selection_sort(arr):
+    comparisons = 0
+    moves = 0
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            comparisons += 1
+            if arr[j] < arr[min_index]:
+                min_index = j 
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        moves += 2  
+    return comparisons, moves
+
 
 
 ## TODO: make Benchmarks
