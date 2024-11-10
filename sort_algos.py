24a118b24662766e0f67e90c09de9955356efebe
hbira <habiraabdelkrim@gmail.com>
2024-11-10 10:27:46 +0100

    adding selection sort

--- sort_algos.py
+++ sort_algos.py
@@ -7,6 +7,24 @@
 ## TODO: Sort Algorithms implementations
 ## TODO: Selection Sort is our task now
 
+def selection_sort(arr):
+    comparisons = 0
+    swaps = 0
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            comparisons += 1
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            swaps += 1
+
+    return  comparisons, swaps
+
 
 ## TODO: make Benchmarks
 
