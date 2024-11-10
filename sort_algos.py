b3222866ac56f6612217b9baa634a86e4b55ae7b
micha <badralboudourlamouri@gmail.com>
2024-11-10 08:51:03 -0800

    adding selection sort task

--- sort_algos.py
+++ sort_algos.py
@@ -10,4 +10,19 @@
 
 ## TODO: make Benchmarks
 
-print('hello')
+import random
+import time
+
+def selection_sort(arr):
+    comparisons = 0
+    moves = 0
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            comparisons += 1
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        moves += 2 
+    return comparisons, moves
