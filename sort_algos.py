80c002367df96f529985aaf16adebfb40d1c6123
wijden04 <widjdenmahcen@gmail.com>
2024-11-10 09:25:38 +0100

    task one of selection sort alogorithm

--- sort_algos.py
+++ sort_algos.py
@@ -6,8 +6,15 @@
 
 ## TODO: Sort Algorithms implementations
 ## TODO: Selection Sort is our task now
-
-
+def selection_sort(unsorted_array):
+    
+    for i in range(0, len(unsorted_array) - 1):
+        Minidx = i
+        
+        for j in range(i + 1, len(unsorted_array)):
+            if unsorted_array[j] < unsorted_array[Minidx]:
+                Minidx = j
+        unsorted_array[i], unsorted_array[Minidx] = unsorted_array[Minidx], unsorted_array[i] # swap
 ## TODO: make Benchmarks
 
 print('hello')
