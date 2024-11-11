1b5d96840ecd08f45edbf19158a6938fcae1010a
Touka-Ha <best.gril33@gmail.com>
2024-11-11 22:08:14 +0100

    selectionSort function

--- sort_algos.py
+++ sort_algos.py
@@ -1,13 +1,14 @@
-## TODO: TP should be HERE
+import random
+def selectionSort(array, size):
+    for i in range(size):
+        min_index = i
+        for j in range(i + 1, size):
+            if array[j] < array[min_index]:
+                min_index = j
+        (array[i], array[min_index]) = (array[min_index], array[i])
 
-
-## TODO: Data Generation
-
-
-## TODO: Sort Algorithms implementations
-## TODO: Selection Sort is our task now
-
-
-## TODO: make Benchmarks
-
-print('hello')
+size = 10
+arr = random.sample(range(-1000, 1000), size)
+selectionSort(arr, size)
+print('The array after sorting in Ascending Order by selection sort is:')
+print(arr)
