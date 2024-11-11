5de7d97e2a1906ad338006703d350b1a8e4ae83c
Chaima <ferdiachaima@gmail.com>
2024-11-11 09:11:24 +0100

    Selection sort

--- sort_algos.py
+++ sort_algos.py
@@ -5,6 +5,16 @@
 
 
 ## TODO: Sort Algorithms implementations
+#Selection Sort
+def selection_sort(array):
+    n = len(array)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if array[j] < array[min_idx]:
+                min_idx = j
+        array[i], array[min_idx] = array[min_idx], array[i]
+    return array
 ## TODO: Selection Sort is our task now
 
 
