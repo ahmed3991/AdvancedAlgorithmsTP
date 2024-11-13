08d0a5d4952ebc6f292fc7a20e3e2f8066196731
yahiahassan <yahiahassan66@gmail.com>
2024-11-13 20:41:45 +0100

    new update for sort selection

--- sort_algos.py
+++ sort_algos.py
@@ -1,28 +1,30 @@
 ## TODO: TP should be HERE
-
+import random
+import time
 
 ## TODO: Data Generation
-
+r = int(input("enter N  =  "))
+data = [random.randint(0, 100) for i in range(r)]
 
 ## TODO: Sort Algorithms implementations
+## TODO: Selection Sort is our task now
 
-def bubble_sort(numbers):
-    n = len(numbers)
+def selectionSort(data, n):
     for i in range(n):
-        
-        already_sorted = True
-        for j in range(n - i - 1):
-            
-            if numbers[j] > numbers[j + 1]:
-               
-                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
-                
-                already_sorted = False
-        
-        if already_sorted:
-            break
-    return numbers
+        min_index = i
+        for j in range(i + 1, n):
+            if data[j] < data[min_index]:
+                min_index = j
+        # Swap the found minimum element with the first element
+        data[i], data[min_index] = data[min_index], data[i]
 
-## TODO: make Benchmarks
-
-print('hello')
+## TODO: Make Benchmarks
+n = len(data)
+print('the table unsort:\n')
+print(data)
+start = time.time()
+selectionSort(data, n)
+end = time.time()
+print('\nThe table after sort:\n')
+print(data)
+print(f"\nTime of sorting: {end - start:} seconds")
