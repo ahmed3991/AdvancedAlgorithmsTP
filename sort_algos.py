6145c0fc48aaf5c4458ec0e3db7b59b6a942cf7e
nacer eddine aouissi <nasseraouissi0@gmail.com>
2024-11-13 16:37:12 +0100

    Selection Sort

--- sort_algos.py
+++ sort_algos.py
@@ -5,8 +5,24 @@
 
 
 ## TODO: Sort Algorithms implementations
-## TODO: Selection Sort is our task now
+## TODO: Selection Sort is our task now\
+def selection_sort(arr):
+    comparison = 0
+    moves = 0
+    arr = arr.copy()
 
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            comparison += 1
+            if arr[j] < arr[min_index]:
+                min_index = j
+        comparison += 1
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            moves += 1
+
+    return comparison, moves
 
 ## TODO: make Benchmarks
 
