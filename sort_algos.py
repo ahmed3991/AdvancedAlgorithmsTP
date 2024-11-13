924cb4dcb12a530805585b5afd47edd5fd26c24f
yahiahassan <yahiahassan66@gmail.com>
2024-11-13 19:15:44 +0100

    sort algorithme

--- sort_algos.py
+++ sort_algos.py
@@ -6,6 +6,22 @@
 
 ## TODO: Sort Algorithms implementations
 
+def bubble_sort(numbers):
+    n = len(numbers)
+    for i in range(n):
+        
+        already_sorted = True
+        for j in range(n - i - 1):
+            
+            if numbers[j] > numbers[j + 1]:
+               
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+                
+                already_sorted = False
+        
+        if already_sorted:
+            break
+    return numbers
 
 ## TODO: make Benchmarks
 
