c7606961a06e81e6422be37fb68eaf3544e4ac98
Houda-Saidi <ne661460@gmail.com>
2024-11-17 10:51:59 +0100

    إضافة التعديلات على sort_algos.py

--- sort_algos.py
+++ sort_algos.py
@@ -6,7 +6,20 @@
 
 ## TODO: Sort Algorithms implementations
 ## TODO: Selection Sort is our task now
-
+def selection_sort_swap(arr):
+    comparisons = 0
+    swaps = 0
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            comparisons += 1
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+            swaps += 1
+    return comparisons, swaps
 
 ## TODO: make Benchmarks
 
