8360451d286540ed09e0913da6f3d4bed36a22aa
Bachir Dridi <doumoua387@gmail.com>
2024-11-17 09:11:28 +0100

    add the sort functions to the reposetry

--- sort_algos.py
+++ sort_algos.py
@@ -26,13 +26,51 @@ def selection_sort(arr):
 ## TODO: Complete the code
 
 def bubble_sort(arr):  
-    pass
+    comparison = 0
+    moves = 0
+    arr = arr.copy()
+
+    for i in range(len(arr)):
+        for j in range(0, len(arr) - i - 1):
+            comparison += 1
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                moves += 1
+
+    return comparison, moves
 
 def insertion_sort_by_shifting(arr):
-    pass
+    comparison = 0
+    moves = 0
+    arr = arr.copy()
+
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            comparison += 1
+            arr[j + 1] = arr[j]
+            j -= 1
+            moves += 1
+        arr[j + 1] = key
+
+    return comparison, moves
 
 def insertion_sort_by_exchanges(arr):
-    pass
+    comparison = 0
+    moves = 0
+    arr = arr.copy()
+
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            comparison += 1
+            if arr[j] < arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+                moves += 1
+            else:
+                break
+
+    return comparison, moves
 
 ## TODO: make Benchmarks
 
