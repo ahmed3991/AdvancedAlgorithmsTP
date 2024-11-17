7a99b13ab0948e092598435f07ed797d7a9990fc
malakguerricha <yocefmalak@gmail.com>
2024-11-17 10:56:13 +0100

    sort algorithm

--- sort_algos.py
+++ sort_algos.py
@@ -25,14 +25,52 @@ def selection_sort(arr):
 
 ## TODO: Complete the code
 
-def bubble_sort(arr):  
-    pass
+def bubble_sort(arr):
+    n = len(arr)
+    comparisons = 0
+    swaps = 0
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            comparisons += 1
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]  # تبادل العناصر
+                swaps += 1
+                swapped = True
+        if not swapped:
+            break  # إذا لم يتم إجراء أي تبادل، فإن المصفوفة مرتبة
+    return arr, comparisons, swaps
 
-def insertion_sort_by_shifting(arr):
-    pass
 
-def insertion_sort_by_exchanges(arr):
-    pass
+ 
+def insertion_sort_shifting(arr):
+    n = len(arr)
+    comparisons = 0
+    shifts = 0
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            comparisons += 1
+            arr[j + 1] = arr[j]  # إزاحة العنصر
+            shifts += 1
+            j -= 1
+        arr[j + 1] = key  # إدخال العنصر في مكانه
+    return arr, comparisons, shifts
+
+def insertion_sort_exchanges(arr):
+    n = len(arr)
+    comparisons = 0
+    swaps = 0
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            comparisons += 1
+            arr[j + 1] = arr[j]  # إزاحة العنصر
+            j -= 1
+        arr[j + 1] = key  # إدخال العنصر في مكانه
+    return arr, comparisons, swaps
 
 ## TODO: make Benchmarks
 
