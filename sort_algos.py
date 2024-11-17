ed40de8b697de6962396250620996aed4f5a5f7d
bdalalydouh <bdalalydouh@gmail.com>
2024-11-17 10:11:04 +0100

    Task 2 selection sort

--- sort_algos.py
+++ sort_algos.py
@@ -2,6 +2,16 @@
 
 
 ## TODO: Data Generation
+np.random.seed(42)
+tests = []
+lengths = np.array(range(100, 1000, 200))
+tests_per_length = 3
+
+for length in lengths:
+        for _ in range(tests_per_length):
+            val = np.random.randint(1, 4 * length, size=length)
+            test = (length, val)
+            tests.append(test)
 
 
 ## TODO: Sort Algorithms implementations
@@ -25,15 +35,65 @@ def selection_sort(arr):
 
 ## TODO: Complete the code
 
-def bubble_sort(arr):  
-    pass
+def  bubble_sort(arr):
+    comparisons = 0
+    swaps = 0
+    n = len(arr)
 
-def insertion_sort_by_shifting(arr):
-    pass
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            comparisons += 1
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swaps += 1
 
-def insertion_sort_by_exchanges(arr):
-    pass
+    return  comparisons, swaps
 
+##time_and_space_profiler
+
+def insertion_sort_shifting(arr):
+    comparisons = 0
+    swaps = 0
+    n = len(arr)
+
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            comparisons += 1
+            arr[j + 1] = arr[j]  # Shift
+            swaps += 1
+            j -= 1
+        arr[j + 1] = key
+
+        if j != i - 1:
+            comparisons += 1
+
+    return comparisons, swaps
+
+##insertion_sort_exchange
+
+def insertion_sort_exchange(arr):
+    comparisons = 0
+    swaps = 0
+    n = len(arr)
+
+    for i in range(1, n):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            comparisons += 1
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]  # Swap
+            swaps += 1
+            j -= 1
+
+        if j > 0:
+            comparisons += 1
+
+    return comparisons, swaps
+
+
+##Convert To Data Frame
 ## TODO: make Benchmarks
 
 print('hello')
