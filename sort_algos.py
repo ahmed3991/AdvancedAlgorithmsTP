dbe5e06e64adc49a4d7792926da39a9bf10b0717
labo <labo@algo.ai>
2024-11-17 08:34:33 +0100

    want to implement

--- sort_algos.py
+++ sort_algos.py
@@ -5,7 +5,6 @@
 
 
 ## TODO: Sort Algorithms implementations
-## TODO: Selection Sort is our task now
 def selection_sort(arr):
     comparison_count = 0
     move_count = 0
@@ -23,7 +22,54 @@ def selection_sort(arr):
             move_count += 1
 
     return comparison_count, move_count
-    
+
+## TODO: Complete the code
+
+def bubble_sort(arr):  
+    i = 0
+    swap = 0
+    comp = 0
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
+            comp = comp + 1
+            if arr[j] > arr[j+1] :
+              c = arr[j+1]
+              arr[j+1] = arr[j]
+              arr[j] = c
+              swap = swap + 1
+
+    return 'sort_bull',comp , swap
+
+def insertion_sort_by_shifting(arr):
+    comp = 0
+    swap = 0
+    for i in range(1,len(arr)):
+          c = arr[i]
+          j = i - 1
+          comp = comp + 1
+          while (j >= 0 and c < arr[j]):
+                comp = comp + 1
+                arr[j+1] = arr[j]
+                j = j - 1
+                swap=swap+1
+          arr[j+1] = c
+    comp = comp - 1
+    return 'sort_insertion' ,comp , swap
+
+def insertion_sort_by_exchanges(arr):
+    i = 0
+    swap = 0
+    comp = 0
+    for i in range(len(arr)):
+        j = i
+        for j in range(len(arr)):
+          comp = comp + 1
+          if arr[j] > arr[i] :
+              c = arr[i]
+              arr[i] = arr[j]
+              arr[j] = c
+              swap = swap + 1
+    return 'sort_selection', comp , swap
 
 ## TODO: make Benchmarks
 
