f32b11e4d248f17e397114b9c46237d7c9b9d43b
hadjer <hadjerzeghidi@gmail.com>
2024-11-17 09:21:07 +0100

    sortesd

--- sort_algos.py
+++ sort_algos.py
@@ -26,14 +26,37 @@ def selection_sort(arr):
 ## TODO: Complete the code
 
 def bubble_sort(arr):  
-    pass
+    
+ n = len(arr)
+    for i in range(n):
+       
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+            
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 def insertion_sort_by_shifting(arr):
-    pass
+
+     n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        # Shift elements of arr[0..i-1], that are greater than key, to one position ahead
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
 
 def insertion_sort_by_exchanges(arr):
-    pass
-
+    
+ n = len(arr)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j-1]:
+                # Swap the elements
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+            else:
+                break
 ## TODO: make Benchmarks
 
 print('hello')
