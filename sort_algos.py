3af68d1281e199c0ce8cc48640ade67805859272
ikram youmbai <ikramyoumba@gmail.com>
2024-11-18 10:10:32 +0100

    this is three algorithm

--- sort_algos.py
+++ sort_algos.py
@@ -26,13 +26,48 @@ def selection_sort(arr):
 ## TODO: Complete the code
 
 def bubble_sort(arr):  
-    pass
+    
+    n = len(arr)
+    for i in range(n - 1): 
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]: 
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+print("Before arranging:", numbers)
+bubble_sort(numbers)
+print("After ranking:", numbers)
+
 
 def insertion_sort_by_shifting(arr):
-    pass
+    for i in range(1, len(arr)):
+        key = arr[i] 
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+print("Before arranging :", numbers)
+insertion_sort_by_shifting(numbers)
+print(" After ranking:", numbers)
+
 
 def insertion_sort_by_exchanges(arr):
-    pass
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]  
+            j -= 1
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+print(" Before arranging:", numbers)
+insertion_sort_by_exchanges(numbers)
+print(" After ranking:", numbers)
+
 
 ## TODO: make Benchmarks
 
