1fe700c4f1a0f0c5b5dc3376d9725e6ec5e5db4d
ahmedlouhidi5 <ahmedbeda1752@gmail.com>
2024-11-18 10:28:12 +0100

    add sort algo

--- sort_algos.py
+++ sort_algos.py
@@ -1,30 +1,84 @@
 ## TODO: TP should be HERE
-
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import time
 
 ## TODO: Data Generation
-
+array_sizes = [1000, 10000, 100000]
+data_types = {"Random": lambda n: np.random.randint(0, n, n),
+              "Ascending": lambda n: np.arange(n),
+              "Descending": lambda n: np.arange(n, 0, -1)}
+# Function to count comparisons and swaps
+def init_profiler():
+    return {"comparisons": 0, "swaps": 0, "start_time": time.time()}
 
 ## TODO: Sort Algorithms implementations
-## TODO: Selection Sort is our task now
+# Selection Sort
 def selection_sort(arr):
-    comparison_count = 0
-    move_count = 0
-    arr = arr.copy()
-
+    profiler = init_profiler()
     for i in range(len(arr)):
-        min_index = i
+        min_idx = i
         for j in range(i + 1, len(arr)):
-            comparison_count += 1
-            if arr[j] < arr[min_index]:
-                min_index = j
-        comparison_count += 1
-        if min_index != i:
-            arr[i], arr[min_index] = arr[min_index], arr[i]
-            move_count += 1
+            profiler["comparisons"] += 1
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        profiler["swaps"] += 1
+    profiler["time"] = time.time() - profiler["start_time"]
+    return profiler
+# Bubble Sort
+def bubble_sort(arr):
+    profiler = init_profiler()
+    for i in range(len(arr)):
+        swapped = False
+        for j in range(len(arr) - 1 - i):
+            profiler["comparisons"] += 1
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                profiler["swaps"] += 1
+                swapped = True
+        if not swapped:
+            break
+    profiler["time"] = time.time() - profiler["start_time"]
+    return profiler
 
-    return comparison_count, move_count
-    
+# Insertion Sort by shifting
+def insertion_sort_shift(arr):
+    profiler = init_profiler()
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            profiler["comparisons"] += 1
+            arr[j + 1] = arr[j]
+            profiler["swaps"] += 1
+            j -= 1
+        arr[j + 1] = key
+    profiler["time"] = time.time() - profiler["start_time"]
+    return profiler
+
 
 ## TODO: make Benchmarks
 
-print('hello')
+results = []
+for size in array_sizes:
+    for label, data_gen in data_types.items():
+        arr = data_gen(size)
+        for sort_func in [selection_sort, bubble_sort, insertion_sort_shift]:
+            
+            data_copy = arr.copy()
+            metrics = sort_func(data_copy)
+            results.append({
+                "algorithm": sort_func.name,
+                "data_type": label,
+                "size": size,
+                "comparisons": metrics["comparisons"],
+                "swaps": metrics["swaps"],
+                "time": metrics["time"]
+            })  
+
+# Output Results
+df = pd.DataFrame(results)
+df.to_csv('sort_benchmarks.csv', index=False)
+print(df)
