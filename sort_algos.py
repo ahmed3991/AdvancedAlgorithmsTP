e28bb3153e3fa2296133ffe2ddc94cff62c94003
Houda-Saidi <ne661460@gmail.com>
2024-11-18 10:11:50 +0100

    The required algorithms

--- sort_algos.py
+++ sort_algos.py
@@ -26,14 +26,45 @@ def selection_sort(arr):
 ## TODO: Complete the code
 
 def bubble_sort(arr):  
-    pass
+
+    n = len(arr)
+    for i in range(n - 1):  
+        for j in range(n - i - 1):  
+            if arr[j] > arr[j + 1]:  
+                arr[j], arr[j + 1] = arr[j + 1], arr
+    print("Bubble Sort Result:", arr)
+
+arr1 = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr1)
+
 
 def insertion_sort_by_shifting(arr):
-    pass
+
+    n = len(arr)
+    for i in range(1, n):  
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key  
+    print("Insertion Sort by Shifting Result:", arr)
+arr2 = [64, 34, 25, 12, 22, 11, 90]
+insertion_sort_by_shifting(arr2)
+
 
 def insertion_sort_by_exchanges(arr):
-    pass
 
-## TODO: make Benchmarks
+    n = len(arr)
+    for i in range(1, n):  
+        for j in range(i, 0, -1):  
+            if arr[j] < arr[j - 1]: 
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            else:
+                break 
+    print("Insertion Sort by Exchanges Result:", arr)
+arr3 = [64, 34, 25, 12, 22, 11, 90]
+insertion_sort_by_exchanges(arr3)
+
 
 print('hello')
