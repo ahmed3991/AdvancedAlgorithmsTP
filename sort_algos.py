6acdcfb3cc0226dad16a20ff1ba165fdf307f25d
mohammdchihani <157797637+mohammdchihani@users.noreply.github.com>
2024-11-21 19:34:31 +0000

    Other Sorts

--- sort_algos.py
+++ sort_algos.py
@@ -4,11 +4,7 @@
 ## TODO: Data Generation
 
 
-## TODO: Sort Algorithms implementations
 def selection_sort(arr):
-    comparison_count = 0
-    move_count = 0
-    arr = arr.copy()
 
     for i in range(len(arr)):
         min_index = i
@@ -19,21 +15,45 @@ def selection_sort(arr):
         comparison_count += 1
         if min_index != i:
             arr[i], arr[min_index] = arr[min_index], arr[i]
-            move_count += 1
 
-    return comparison_count, move_count
+    return arr
 
-## TODO: Complete the code
 
-def bubble_sort(arr):  
-    pass
+def Bubble_sort(array,length):
+    count = 0
 
-def insertion_sort_by_shifting(arr):
-    pass
+    for i in range(length):
+        for j in range(0,length - i-1):
+            if array[j] > array[j+1]:
+                swap = array[j]
+                array[j] = array[j+1]
+                array[j+1] = swap
+                count += 1
 
-def insertion_sort_by_exchanges(arr):
-    pass
+    return array
 
-## TODO: make Benchmarks
 
-print('hello')
+def insertion_sort_by_shifting(arr,length):
+    count = 0 
+
+    for i in range(length-1):
+        j = i
+        while j >= 0 and arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            j -= 1
+            count += 1
+
+    return arr
+
+
+def insertion_sort_by_exchanges(arr,length):
+    count = 0 
+
+    for i in range(1, length):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]  
+            count += 1  
+            j -= 1
+    
+    return arr
