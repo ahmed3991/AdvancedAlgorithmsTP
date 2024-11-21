fa8fdd2741fcf3bf362a25a5da29c917bd05fbb4
raniaarb <raniaarb19@gmail.com>
2024-11-21 19:52:13 +0100

    adding sorts

--- sort_algos.py
+++ sort_algos.py
@@ -25,15 +25,37 @@ def selection_sort(arr):
 
 ## TODO: Complete the code
 
-def bubble_sort(arr):  
-    pass
-
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # متغير لتتبع ما إذا حدث تبادل
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                # تبديل العناصر إذا كانت في ترتيب خاطئ
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        # إذا لم يحدث أي تبادل، المصفوفة مرتبة
+        if not swapped:
+            break
 def insertion_sort_by_shifting(arr):
-    pass
-
+  def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
 def insertion_sort_by_exchanges(arr):
-    pass
+    def insertion_sort_by_exchange(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]  # التبديل
+            j -= 1
+    return arr
 
 ## TODO: make Benchmarks
 
-print('hello')
