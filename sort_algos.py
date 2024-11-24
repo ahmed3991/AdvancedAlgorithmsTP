2fa4765576d2c31b77d5445f2f02bd8400db2c4b
BENDEBKA MANAR SAMA <manarbendebka@glmail.com>
2024-11-24 09:37:34 +0100

    adding testing and results

--- sort_algos.py
+++ sort_algos.py
@@ -1,17 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 ## TODO: TP should be HERE
 
 
 ## TODO: Data Generation
 
-lenghts =[10,100,1000,10000]
-
+lengths =[10,100,1000,10000]
 # TODO : Use numpy
-random_arrays= []
+random_arrays= [np.random.randint(0, 10000, size=l).tolist() for l in lengths]
 # TODO : Use range
-sorted_arrays= []
+sorted_arrays= [list(range(l)) for l in lengths]
 # TODO : Use range
-inverse_sorted_arrays = []
-
+inverse_sorted_arrays = [list(range(l, 0, -1)) for l in lengths]
 nbr_experiments = 10
 
 
@@ -91,7 +93,78 @@ def insertion_sort_exchange(arr):
 
 
 funcs = [selection_sort, bubble_sort,insertion_sort_shifting,insertion_sort_exchange]
-
 results = []
- 
+
+for func in funcs:
+    print(f"Testing {func.__name__}")
+    for l, (rand_arr, sorted_arr, inv_sorted_arr) in enumerate(zip(random_arrays, sorted_arrays, inverse_sorted_arrays)):
+        print(f"Array length: {lengths[l]}")
+        rand_comparisons = 0
+        rand_swaps = 0
+
+        sorted_comparisons = 0
+        sorted_swaps = 0
+
+        inv_comparisons = 0
+        inv_swaps = 0
+
+        for _ in range(nbr_experiments):
+            comp, swap = func(rand_arr.copy())
+            rand_comparisons += comp
+            rand_swaps += swap
+
+        comp, swap = func(sorted_arr.copy())
+        sorted_comparisons += comp
+        sorted_swaps += swap
+
+        comp, swap = func(inv_sorted_arr.copy())
+        inv_comparisons += comp
+        inv_swaps += swap
+
+        results.append({
+            "algorithm": func.__name__,
+            "array_length": lengths[l],
+            "random_comparisons": rand_comparisons / nbr_experiments,
+            "random_swaps": rand_swaps / nbr_experiments,
+            "sorted_comparisons": sorted_comparisons,
+            "sorted_swaps": sorted_swaps,
+            "inverse_comparisons": inv_comparisons,
+            "inverse_swaps": inv_swaps
+        })
+
+import pandas as pd
+df = pd.DataFrame(results)
+print(df)
+array_lengths = list(set([result['array_length'] for result in results]))
+algorithms = list(set([result['algorithm'] for result in results]))
+
+plt.figure(figsize=(10, 6))
+for algo in algorithms:
+    comparisons = [
+        result['random_comparisons']
+        for result in results if result['algorithm'] == algo
+    ]
+    plt.plot(array_lengths, comparisons, label=f"{algo} (Comparisons)", marker='o')
+
+plt.title('Comparisons vs Array Lengths')
+plt.xlabel('Array Length')
+plt.ylabel('Number of Comparisons')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+for algo in algorithms:
+    swaps = [
+        result['random_swaps']
+        for result in results if result['algorithm'] == algo
+    ]
+    plt.plot(array_lengths, swaps, label=f"{algo} (Swaps)", marker='o')
+
+plt.title('Swaps vs Array Lengths')
+plt.xlabel('Array Length')
+plt.ylabel('Number of Swaps')
+plt.legend()
+plt.grid(True)
+plt.show()
 # TODO: Complete the benchmark code
