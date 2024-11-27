3c1e024a2a65c1aeb8b87c3c1af66efd335ef978
Guedda Nour <gueddanour8@gmail.com>
2024-11-27 08:49:45 -0800

    DONE

--- sort_algos.py
+++ sort_algos.py
@@ -3,7 +3,7 @@
 
 ## TODO: Data Generation
 
-
+#fhfhfh
 ## TODO: Sort Algorithms implementations
 def selection_sort(arr):
     comparison_count = 0
@@ -25,14 +25,54 @@ def selection_sort(arr):
 
 ## TODO: Complete the code
 
-def bubble_sort(arr):  
-    pass
-
-def insertion_sort_by_shifting(arr):
-    pass
+def bubble_sort(arr):
+    comparisons = 0
+    moves = 0
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            comparisons += 1
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                moves += 1
+                swapped = True
+        if not swapped:
+            break
+    return comparisons, moves
 
 def insertion_sort_by_exchanges(arr):
-    pass
+    comparisons = 0
+    moves = 0
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            comparisons += 1
+            arr[j + 1] = arr[j]
+            moves += 1
+            j -= 1
+        arr[j + 1] = key
+        moves += 1
+    return comparisons, moves
+
+def insertion_sort_by_shifting(arr):
+    comparisons = 0
+    moves = 0
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            comparisons += 1
+            j -= 1
+        arr[j + 1] = key
+        moves += 1
+    return comparisons, moves
+
+
+
 
 ## TODO: make Benchmarks
 
