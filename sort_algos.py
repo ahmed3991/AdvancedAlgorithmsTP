8ead9a8f5ed1a73399ed713a644a05c6ee9ca7e1
Touka-Ha <best.gril33@gmail.com>
2024-11-27 13:08:52 +0100

    refactoring

--- sort_algos.py
+++ sort_algos.py
@@ -1,14 +1,14 @@
 import random
-def selectionSort(array, size):
-    for i in range(size):
+def selectionSort(array, size_array):
+    for i in range(size_array):
         min_index = i
-        for j in range(i + 1, size):
+        for j in range(i + 1, size_array):
             if array[j] < array[min_index]:
                 min_index = j
         (array[i], array[min_index]) = (array[min_index], array[i])
 
-size = 10
-arr = random.sample(range(-1000, 1000), size)
-selectionSort(arr, size)
+size_array = 10
+arr = random.sample(range(-1000, 1000), size_array)
+selectionSort(arr, size_array)
 print('The array after sorting in Ascending Order by selection sort is:')
 print(arr)
