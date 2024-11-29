b47367f40332a49b011cfcacec5491972abd45c5
MalakHch <malakharr29@gmail.com>
2024-11-29 18:58:17 +0100

    Add: Other Sorts

--- sort_algos.py
+++ sort_algos.py
@@ -1,84 +1,104 @@
 ## TODO: TP should be HERE
+import random
+import time
 
 
 ## TODO: Data Generation
+r = int(input("Enter the range of data: "))
+data = [random.randint(0, 10000) for i in range(r)]
 
 
 ## TODO: Sort Algorithms implementations
-def selection_sort(arr):
-    comparison_count = 0
-    move_count = 0
-    arr = arr.copy()
-
-    for i in range(len(arr)):
-        min_index = i
-        for j in range(i + 1, len(arr)):
-            comparison_count += 1
-            if arr[j] < arr[min_index]:
-                min_index = j
-        comparison_count += 1
-        if min_index != i:
-            arr[i], arr[min_index] = arr[min_index], arr[i]
-            move_count += 1
-
-    return comparison_count, move_count
+## TODO: Selection Sort is our task now
+def selectionSort(data):
+    comps = 0
+    swaps = 0
+    for i in range(len(data)):
+        min=i
+        for j in range(i + 1, len(data)):
+            comps += 1
+            if data[j] < data[min]:
+                min=j
+        if min!= i:
+            data[i], data[min] = data[min], data[i]
+            swaps += 1
+    return comps, swaps
 
 ## TODO: Complete the code
 
-def bubble_sort(arr):
-    comparisons = 0
+def bubbleSort(data):
+    comps = 0
     swaps = 0
-    n = len(arr)
-
+    n = len(data)
     for i in range(n):
         for j in range(0, n - i - 1):
-            comparisons += 1
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            comps += 1
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
                 swaps += 1
+    return comps, swaps
 
-    return  comparisons, swaps
-
-
-def insertion_sort_shifting(arr):
-    comparisons = 0
-    swaps = 0
-    n = len(arr)
+def insertionSortShifting(data,n):
+    comps=0
+    swaps=0
 
     for i in range(1, n):
-        key = arr[i]
+        temp = data[i]
         j = i - 1
 
-        while j >= 0 and arr[j] > key:
-            comparisons += 1
-            arr[j + 1] = arr[j]  # Shift
+        while j >= 0 and data[j] > temp:
+            comps+ 1
+            data[j+1] = data[j]
             swaps += 1
             j -= 1
-        arr[j + 1] = key
+        data[j+1] = temp
 
-        if j != i - 1:
-            comparisons += 1
+        if j!= i-1:
+            comps=1
 
-    return  comparisons, swaps
-def insertion_sort_exchange(arr):
-    comparisons = 0
-    swaps = 0
-    n = len(arr)
+    return comps ,swaps
+
+def insertionSortExchange(data,n):
+    comps=0
+    swaps=0
 
     for i in range(1, n):
-        j = i
-        while j > 0 and arr[j] < arr[j - 1]:
-            comparisons += 1
-            arr[j], arr[j - 1] = arr[j - 1], arr[j]  # Swap
-            swaps += 1
-            j -= 1
+        j=i
+        while j>0 and data[j]<data[j-1]:
+            comps += 1
+            data[j], data[j-1] = data[j-1], data[j]
+            swaps+=1
+            j-=1
 
-        if j > 0:
-            comparisons += 1
+        if j>0:
+            comps+=1
 
-    return  comparisons, swaps
+    return comps, swaps
 
 
 ## TODO: make Benchmarks
 
-print('hello')
+def benchmarkSort(sort_function, data):
+    start = time.time()
+    comps, swaps = sort_function(data)
+    end = time.time()
+    return comps, swaps, end - start
+
+print('Data before sorting:')
+print(data[:20])
+
+comps, swaps, duration = benchmarkSort(selectionSort, data.copy())
+print("\nSelection Sort:")
+print(f"Comparisons: {comps}, Swaps: {swaps}, Time: {duration:.5f} seconds")
+
+comps, swaps, duration = benchmarkSort(bubbleSort, data.copy())
+print("\nBubble Sort:")
+print(f"Comparisons: {comps}, Swaps: {swaps}, Time: {duration:.5f} seconds")
+
+comps, swaps, duration = benchmarkSort(insertionSortShifting, data.copy())
+print("\nInsertion Sort (Shifting):")
+print(f"Comparisons: {comps}, Swaps: {swaps}, Time: {duration:.5f} seconds")
+
+comps, swaps, duration = benchmarkSort(insertionSortExchange, data.copy())
+print("\nInsertion Sort (Exchange):")
+print(f"Comparisons: {comps}, Swaps: {swaps}, Time: {duration:.5f} seconds")
