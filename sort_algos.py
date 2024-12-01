42423fd846470f4f285325c412787202ab86d499
Touka-Ha <best.gril33@gmail.com>
2024-12-01 23:30:03 +0100

    Task3...

--- sort_algos.py
+++ sort_algos.py
@@ -1,5 +1,5 @@
 ## TODO: TP should be HERE
-
+import numpy as np
 
 ## TODO: Data Generation
 
@@ -16,25 +16,24 @@ nbr_experiments = 10
 
 
 def selection_sort(arr):
-    comparison_count = 0
-    move_count = 0
+    comparisons = 0
+    swaps = 0
     arr = arr.copy()
 
     for i in range(len(arr)):
         min_index = i
         for j in range(i + 1, len(arr)):
-            comparison_count += 1
+            comparisons += 1
             if arr[j] < arr[min_index]:
                 min_index = j
-        comparison_count += 1
+        comparisons += 1
         if min_index != i:
             arr[i], arr[min_index] = arr[min_index], arr[i]
-            move_count += 1
+            swaps += 1
 
-    return comparison_count, move_count
+    return comparisons, swaps
 
 ## TODO: Complete the code
-
 def bubble_sort(arr):
     comparisons = 0
     swaps = 0
@@ -95,3 +94,25 @@ funcs = [selection_sort, bubble_sort,insertion_sort_shifting,insertion_sort_exch
 results = []
  
 # TODO: Complete the benchmark code
+random_arrays = [np.random.randint(0, 1000, size=n).tolist() for n in lenghts]
+sorted_arrays = [list(range(n)) for n in lenghts]
+inverse_sorted_arrays = [list(range(n, 0, -1)) for n in lenghts]
+for func in funcs:
+    for arr_type, arrays in zip(["Random", "Sorted", "Inverse Sorted"],
+                                [random_arrays, sorted_arrays, inverse_sorted_arrays]):
+        for n, base_array in zip(lenghts, arrays):
+            total_comparisons = 0
+            total_swaps = 0
+            for _ in range(nbr_experiments):
+                comparisons, swaps = func(base_array)
+                total_comparisons += comparisons
+                total_swaps += swaps
+            avg_comparisons = total_comparisons / nbr_experiments
+            avg_swaps = total_swaps / nbr_experiments
+            results.append({
+                "Algorithm": func.__name__,
+                "Array Type": arr_type,
+                "Length": n,
+                "Average Comparisons": avg_comparisons,
+                "Average Swaps": avg_swaps
+            })
