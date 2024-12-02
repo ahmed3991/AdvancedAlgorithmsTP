4958d6e5fb5943f427fe109b0a51d5f5dfe43c71
raniaarb <raniaarb19@gmail.com>
2024-12-02 18:18:21 +0100

    create arrays

--- sort_algos.py
+++ sort_algos.py
@@ -3,51 +3,109 @@
 
 ## TODO: Data Generation
 
+lenghts =[10,100,1000,10000]
 
-## TODO: Sort Algorithms implementations
+# TODO : Use numpy
+random_arrays= []
+import numpy as np
+array_10 = np.random.rand(10)
+array_100 = np.random.rand(100)
+array_1000 = np.random.rand(1000)
+array_10000 = np.random.rand(10000)
+
+sorted_arrays= []
+import numpy as np
+array_10 = np.linspace(1, 10, 10) 
+array_100 = np.linspace(1, 100, 100)  
+array_1000 = np.linspace(1, 1000, 1000)  
+array_10000 = np.linspace(1, 10000, 10000)  
+
+inverse_sorted_arrays = []
+import numpy as np
+array_10 = np.linspace(1, 10, 10)[::-1] 
+array_100 = np.linspace(1, 100, 100)[::-1] 
+array_1000 = np.linspace(1, 1000, 1000)[::-1]  
+array_10000 = np.linspace(1, 10000, 10000)[::-1] 
+
+
+
 def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        # Find the minimum element in remaining unsorted array
-        min_idx = i
-        for j in range(i + 1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
+    comparison_count = 0
+    move_count = 0
+    arr = arr.copy()
 
-        # Swap the found minimum element with the first element
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            comparison_count += 1
+            if arr[j] < arr[min_index]:
+                min_index = j
+        comparison_count += 1
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            move_count += 1
+
+    return comparison_count, move_count
 
 ## TODO: Complete the code
 
 def bubble_sort(arr):
+    comparisons = 0
+    swaps = 0
     n = len(arr)
+
     for i in range(n):
-        # متغير لتتبع ما إذا حدث تبادل
-        swapped = False
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                # تبديل العناصر إذا كانت في ترتيب خاطئ
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swapped = True
-        # إذا لم يحدث أي تبادل، المصفوفة مرتبة
-        if not swapped:
-            break
-def insertion_sort_by_shifting(arr):
-   for i in range(1, len(arr)):
+        for j in range(0, n - i - 1):
+            comparisons += 1
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swaps += 1
+
+    return  comparisons, swaps
+
+
+def insertion_sort_shifting(arr):
+    comparisons = 0
+    swaps = 0
+    n = len(arr)
+
+    for i in range(1, n):
         key = arr[i]
         j = i - 1
+
         while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
+            comparisons += 1
+            arr[j + 1] = arr[j]  # Shift
+            swaps += 1
             j -= 1
         arr[j + 1] = key
-    return arr
-    
-def insertion_sort_by_exchange(arr):
-    for i in range(1, len(arr)):
+
+        if j != i - 1:
+            comparisons += 1
+
+    return  comparisons, swaps
+def insertion_sort_exchange(arr):
+    comparisons = 0
+    swaps = 0
+    n = len(arr)
+
+    for i in range(1, n):
         j = i
         while j > 0 and arr[j] < arr[j - 1]:
-            arr[j], arr[j - 1] = arr[j - 1], arr[j]  # التبديل
+            comparisons += 1
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]  # Swap
+            swaps += 1
             j -= 1
-    return arr
+
+        if j > 0:
+            comparisons += 1
+
+    return  comparisons, swaps
 
 
+
+funcs = [selection_sort, bubble_sort,insertion_sort_shifting,insertion_sort_exchange]
+
+results = []
+ 
+# TODO: Complete the benchmark code
