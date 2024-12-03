446d1bdd8fbadd0a8ef2fb4208eb077b8e742b59
Chaima <ferdiachaima@gmail.com>
2024-12-03 21:47:48 +0100

    Complete the merge sort

--- examples/sort_algos_extended.py
+++ examples/sort_algos_extended.py
@@ -70,7 +70,46 @@ def selection_sort(arr):
 ## TODO: Complete the merge sort
 
 def merge_sort(arr):
-    pass
+    comparisons = 0
+    swaps = 0
+
+    if len(arr) > 1:
+
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        left_half, left_comparisons, left_swaps = merge_sort(left_half)
+        right_half, right_comparisons, right_swaps = merge_sort(right_half)
+
+        comparisons += left_comparisons + right_comparisons
+        swaps += left_swaps + right_swaps
+
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            comparisons += 1  
+            if left_half[i] >= right_half[j]:   
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            swaps += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+            swaps += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+            swaps += 1  
+
+    return arr, comparisons, swaps
 
 # Algorithms to benchmark
 funcs = [
