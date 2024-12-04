d80faf7c104f17966da66e787066da6cdd382d4a
bachir <bachirmeftah35@gmail.com>
2024-12-04 21:30:07 +0100

    Task3/sorts execution

--- sort_algos.py
+++ sort_algos.py
@@ -1,9 +1,11 @@
-## TODO: TP should be HERE
+import numpy as np
+import time
+import tracemalloc
 
-
+import pandas as pd
 ## TODO: Data Generation
 
-lenghts =[10,100,1000,10000]
+lenghts =[10,100,1000]
 
 # TODO : Use numpy
 random_arrays= []
@@ -12,7 +14,7 @@ sorted_arrays= []
 # TODO : Use range
 inverse_sorted_arrays = []
 
-nbr_experiments = 10
+nbr_experiments = 1
 
 
 def selection_sort(arr):
@@ -95,3 +97,43 @@ funcs = [selection_sort, bubble_sort,insertion_sort_shifting,insertion_sort_exch
 results = []
  
 # TODO: Complete the benchmark code
+random_arrays = [np.random.randint(0, 1000, size=n).tolist() for n in lenghts]
+sorted_arrays = [list(range(n)) for n in lenghts]
+inverse_sorted_arrays = [list(range(n, 0, -1)) for n in lenghts]
+for func in funcs:
+    for arr_type, arrays in zip(["Random", "Sorted", "Inverse Sorted"],
+                                [random_arrays, sorted_arrays, inverse_sorted_arrays]):
+        for n, base_array in zip(lenghts, arrays):
+            total_comparisons = 0
+            total_swaps = 0
+            total_time = 0
+            total_memory = 0
+            for _ in range(nbr_experiments):
+                tracemalloc.start()
+                start_time = time.perf_counter()
+
+                comparisons, swaps = func(base_array)
+
+                end_time = time.perf_counter()
+                _, peak_memory = tracemalloc.get_traced_memory()
+                tracemalloc.stop()
+                total_comparisons += comparisons
+                total_swaps += swaps
+                total_time += (end_time - start_time)
+                total_memory += peak_memory
+            avg_comparisons = total_comparisons / nbr_experiments
+            avg_swaps = total_swaps / nbr_experiments
+            avg_time = total_time / nbr_experiments
+            avg_memory = total_memory / nbr_experiments
+            results.append({
+                "Algorithm": func.__name__,
+                "Array Type": arr_type,
+                "Length": n,
+                "Average Comparisons": avg_comparisons,
+                "Average Swaps": avg_swaps,
+                "Average Time (s)": avg_time,
+                "Average Memory (KB)": avg_memory / 1024
+            })
+df = pd.DataFrame(results)
+print(df)
+df.to_csv("sorting_algorithms_results.csv", index=False)
