a07d3943f0598615f1f170c4b552070f8f87f2ac
ridhamsi <61561154+ridhamsi@users.noreply.github.com>
2024-12-07 23:43:53 +0100

    task 1

--- sort_algos.py
+++ sort_algos.py
@@ -1,30 +1,14 @@
-## TODO: TP should be HERE
-
-
-## TODO: Data Generation
-
-
-## TODO: Sort Algorithms implementations
-## TODO: Selection Sort is our task now
-def selection_sort(arr):
-    comparison_count = 0
-    move_count = 0
-    arr = arr.copy()
-
-    for i in range(len(arr)):
+import random
+def selectionSort(array, size_array):
+    for i in range(size_array):
         min_index = i
-        for j in range(i + 1, len(arr)):
-            comparison_count += 1
-            if arr[j] < arr[min_index]:
+        for j in range(i + 1, size_array):
+            if array[j] < array[min_index]:
                 min_index = j
-        comparison_count += 1
-        if min_index != i:
-            arr[i], arr[min_index] = arr[min_index], arr[i]
-            move_count += 1
+        (array[i], array[min_index]) = (array[min_index], array[i])
 
-    return comparison_count, move_count
-    
-
-## TODO: make Benchmarks
-
-print('hello')
+size_array = 10
+arr = random.sample(range(-1000, 1000), size_array)
+selectionSort(arr, size_array)
+print('The array after sorting in Ascending Order by selection sort is:')
+print(arr)
