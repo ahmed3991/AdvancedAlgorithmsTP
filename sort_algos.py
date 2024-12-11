6b2832c5b00bf9ae76d833dfbfefe16ca6408711
rimchiba09 <chebarim3@gmail.com>
2024-12-11 22:17:12 +0100

    sort_algos

--- sort_algos.py
+++ sort_algos.py
@@ -1,19 +1,15 @@
-## TODO: TP should be HERE
+import numpy as np
+import matplotlib.pyplot as plt
 
+lengths = [10, 100, 1000, 10000]
 
-## TODO: Data Generation
+random_arrays = [np.random.randint(0, 100, size=l).tolist() for l in lengths]
 
-lenghts =[10,100,1000,10000]
+sorted_arrays = [list(range(l)) for l in lengths]
 
-# TODO : Use numpy
-random_arrays= []
-# TODO : Use range
-sorted_arrays= []
-# TODO : Use range
-inverse_sorted_arrays = []
+inverse_sorted_arrays = [list(range(l, 0, -1)) for l in lengths]
 
 nbr_experiments = 10
-
 
 def selection_sort(arr):
     comparison_count = 0
@@ -33,7 +29,6 @@ def selection_sort(arr):
 
     return comparison_count, move_count
 
-## TODO: Complete the code
 
 def bubble_sort(arr):
     comparisons = 0
@@ -47,7 +42,7 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swaps += 1
 
-    return  comparisons, swaps
+    return comparisons, swaps
 
 
 def insertion_sort_shifting(arr):
@@ -61,7 +56,7 @@ def insertion_sort_shifting(arr):
 
         while j >= 0 and arr[j] > key:
             comparisons += 1
-            arr[j + 1] = arr[j]  # Shift
+            arr[j + 1] = arr[j] 
             swaps += 1
             j -= 1
         arr[j + 1] = key
@@ -69,7 +64,9 @@ def insertion_sort_shifting(arr):
         if j != i - 1:
             comparisons += 1
 
-    return  comparisons, swaps
+    return comparisons, swaps
+
+
 def insertion_sort_exchange(arr):
     comparisons = 0
     swaps = 0
@@ -79,19 +76,48 @@ def insertion_sort_exchange(arr):
         j = i
         while j > 0 and arr[j] < arr[j - 1]:
             comparisons += 1
-            arr[j], arr[j - 1] = arr[j - 1], arr[j]  # Swap
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]  
             swaps += 1
             j -= 1
 
         if j > 0:
             comparisons += 1
 
-    return  comparisons, swaps
+    return comparisons, swaps
 
 
+funcs = [selection_sort, bubble_sort, insertion_sort_shifting, insertion_sort_exchange]
+results = {func.__name__: {"Random": [], "Sorted": [], "Inverse Sorted": []} for func in funcs}
 
-funcs = [selection_sort, bubble_sort,insertion_sort_shifting,insertion_sort_exchange]
+for func in funcs:
+    for arr_type, arrays in zip(
+        ["Random", "Sorted", "Inverse Sorted"], 
+        [random_arrays, sorted_arrays, inverse_sorted_arrays]
+    ):
+        for arr in arrays:
+            comparisons, swaps = func(arr)
+            results[func.__name__][arr_type].append((len(arr), comparisons, swaps))
 
-results = []
- 
-# TODO: Complete the benchmark code
+n_cols = 3
+n_rows = (len(funcs) + n_cols - 1) // n_cols
+fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5 * n_rows))
+
+axes = axes.flatten()
+
+for i, func in enumerate(funcs):
+    ax = axes[i]
+    for arr_type in ["Random", "Sorted", "Inverse Sorted"]:
+        lengths = [r[0] for r in results[func.__name__][arr_type]]
+        comparisons = [r[1] for r in results[func.__name__][arr_type]]
+        ax.plot(lengths, comparisons, label=f"{arr_type} Arrays")
+
+    ax.set_title(f"{func.__name__} - Comparisons vs Array Length")
+    ax.set_xlabel("Array Length")
+    ax.set_ylabel("Number of Comparisons")
+    ax.legend()
+
+for j in range(len(funcs), len(axes)):
+    fig.delaxes(axes[j])
+
+plt.tight_layout()
+plt.show()
